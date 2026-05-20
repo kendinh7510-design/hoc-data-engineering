b9a1a74ae138a8f37bd08fe1d517849d75ddc3fa
kendinh7510 <kendinh7510@gmail.com>
2026-05-20 04:40:13 -0500

    first exercise

---
+++ tuan_1/bai1.py
@@ -0,0 +1,16 @@
+# Viết chương trình tính tiền lương:
+
+# Lương cơ bản: 10,000,000 đồng
+# Số ngày làm việc: 22 ngày
+# Số ngày thực tế đi làm: 20 ngày
+# Tính lương thực nhận và in ra màn hình
+
+# Kết quả mong đợi:
+# Luong thuc nhan: 9090909 dong
+
+basicLabor = 10000000
+workingDays =22
+actualWorkingDays = 20
+actualLabor = 0
+actualLabor = (basicLabor / workingDays) * actualWorkingDays
+print("Luong nhan:", actualLabor, "dong")
